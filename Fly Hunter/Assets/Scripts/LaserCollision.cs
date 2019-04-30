cc062a090b4f8491b38b884ae9727adacba330c2
LWIZzyouNi <clement.boutevin17340@gmail.com>
2019-04-30 10:58:02 +0200

    *.*

---
+++ Fly Hunter/Assets/Scripts/LaserCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class LaserCollision : MonoBehaviour {
+
+    public GameObject objetVisee;
+    public Interactable interactable;
+
+	// Use this for initialization
+	void Start () {
+        interactable = objetVisee.GetComponent<Interactable>(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.tag == "InteractableObject")
+        {
+            interactable.highlightOnHover = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (gameObject.tag == "InteractableObject")
+        {
+            interactable.highlightOnHover = false;
+        }
+    }
+}
