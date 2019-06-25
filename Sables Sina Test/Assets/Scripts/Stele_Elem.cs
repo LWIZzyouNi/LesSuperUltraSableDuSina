e5c215010150d66d69561a939a56660a9ab6dc8f
Unamycs <t.rousselin@orange.fr>
2019-06-25 10:43:42 +0200

    Stele, élément tout ça tout ça mais c'est que le début.

---
+++ Sables Sina Test/Assets/Scripts/Stele_Elem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stele_Elem : MonoBehaviour {
+
+    public GameObject[] element;
+
+    private int randomNumber = 0;
+
+    // Use this for initialization
+    void Start () {
+        getChild();
+        ActiveChild();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void getChild()
+    {
+        element = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            element[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void ActiveChild()
+    {
+        randomNumber = Random.Range(0, element.Length);
+
+        element[randomNumber].SetActive(true);
+    }
+}
